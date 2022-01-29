7686c3e258092583cbe9b64bba4d5eeff2a4638a
sarjis-abdullah <sarjis@reformedtech.org>
2022-01-29 16:26:20 +0600

    put method worked and pushing it now

--- src/components/category/Index.js
+++ src/components/category/Index.js
@@ -1,29 +1,48 @@
 import React, { useState, useEffect } from "react";
-import SingleItem from './SingleItem';
-import axios from 'axios';
-
+import SingleItem from "./SingleItem";
+import axios from "axios";
 
 const BASE_URL = "http://localhost:3001/";
 function Index() {
   const [cats, setCats] = useState([]);
-  const deleteItem = (id)=> {
-    setCats(items=> items.filter(item=> item.id !== id))
-    axios.delete(BASE_URL + "category/" + id)
+  const deleteItem = (id) => {
+    setCats((items) => items.filter((item) => item.id !== id));
+    axios.delete(BASE_URL + "category/" + id);
+  };
+  const updateItem = (updatedItem)=> {
+    console.log(updatedItem);
+    let items = cats.map((item) => {
+      if(item.id === updatedItem.id){
+        return{
+          ...updatedItem,
+          name: updatedItem.name,
+          description: updatedItem.description,
+        }
+      }
+      return item;
+    })
+    setCats(data=> items);
+    axios.put(BASE_URL + "category/" + updatedItem.id, updatedItem)
   }
   useEffect(() => {
     let respons = fetch(BASE_URL + "category");
     respons
       .then((res) => {
-        return res.json()
+        return res.json();
       })
       .then((d) => {
-        setCats(d.data)
+        setCats(d.data);
       });
   }, []);
 
   return (
     <div className="container">
-      <header className="App-header">MERN Web App</header>
+      <section className="table-header">
+        <header className="App-header">MERN Web App</header>
+        <div className="justify-self-end">
+          <button className="primary-btn">Add New Category</button>
+        </div>
+      </section>
       <table className="w-full">
         <thead className="table--header">
           <tr className="table--header-row">
@@ -33,9 +52,20 @@ function Index() {
           </tr>
         </thead>
         <tbody className="table--body">
-          {cats && cats.length ? cats.map((item, index) => (
-            <SingleItem deleteItem={({id})=> deleteItem(id)} item={item} key={index}/>
-          )) : (<tr><td>Loading...</td></tr>)}
+          {cats && cats.length ? (
+            cats.map((item, index) => (
+              <SingleItem
+                deleteItem={({ id }) => deleteItem(id)}
+                updateItem={(item) => updateItem(item)}
+                item={item}
+                key={index}
+              />
+            ))
+          ) : (
+            <tr>
+              <td>Loading...</td>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
--- src/components/category/SingleItem.js
+++ src/components/category/SingleItem.js
@@ -1,35 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./category.css";
 
 export default function SingleItem(props) {
-    let {item, deleteItem} = props
+  let { item, deleteItem, updateItem } = props;
   const [editMode, setEditMode] = useState(false);
-  const handleName = (e) => {
-    item.name = e.target.value;
-    console.log(item.name, e.target.value);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  
+  useEffect(() => {
+      if (item && Object.keys(item).length) {
+        setName(item.name)
+        setDescription(item.description)
+      }
+  }, []);
+
+  const handleChanges = (e) => {
+    const val = e.target.value
+    if (e.target.name === "name") {
+      setName(val)
+    }else if(e.target.name === "description"){
+      setDescription(val)
+    }
   };
+  const toggleEditMode = ()=> {
+    setEditMode((mode) => !mode);
+  }
+  const update = (item)=> {
+    console.log("Updated", item);
+    toggleEditMode();
+    updateItem({
+      ...item,
+      description,
+      name
+    })
+  }
+  
   return (
     <tr className="table--row">
       <td className="table--row__data">
         {editMode ? (
-          <input value={item.name} onChange={(e) => handleName(e)} />
+          <input value={name} name="name" onChange={(e) => handleChanges(e)} />
         ) : (
           <span>{item.name}</span>
         )}
       </td>
       <td className="table--row__data">
         {editMode ? (
-          <input value={item.description || ""} onChange={(e) => handleName(e)} />
+          <input
+            value={description || ""}
+            name="description"
+            onChange={(e) => handleChanges(e)}
+          />
         ) : (
           <span>{item.description}</span>
         )}
       </td>
       <td className="table--row__data">
         <div className="htree-dot-icon">
-          <button onClick={() => setEditMode((mode) => !mode)}>
+          <button
+            className="secondary-btn"
+            onClick={() => editMode? update(item) : toggleEditMode()}
+          >
             {editMode ? "Update" : "Edit"}
           </button>
-          <button onClick={()=>deleteItem(item)}>Delete</button>
+          <button className="danger-btn" onClick={() => deleteItem(item)}>
+            Delete
+          </button>
           {/* <input type="checkbox" className="hidden toggle" id="1"/>
             <label for="1">hello</label>
             <div className="hidden toggle__menu">
